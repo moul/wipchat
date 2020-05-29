e1b26e4bbc7cd94be21dbfb463e2dfcf2e3cc7be
Manfred Touron <94029+moul@users.noreply.github.com>
2020-05-30 00:37:46 +0200

    chore: add basic unit tests

--- wipchat.go
+++ wipchat.go
@@ -43,68 +43,79 @@ func (c Client) QueryViewer(ctx context.Context) (*wiptypes.ViewerQuery, error) 
 	return &query, nil
 }
 
+func (c Client) uploadAttachment(ctx context.Context, attachment Attachment) (*wiptypes.AttachmentInput, error) {
+	if !c.hasKey {
+		return nil, ErrTokenRequired
+	}
+	var mutation wiptypes.CreatePresignedURLMutation
+	variables := map[string]interface{}{
+		"filename": graphql.String(attachment.Filename),
+	}
+	err := c.graphql.Mutate(ctx, &mutation, variables)
+	if err != nil {
+		return nil, err
+	}
+	if mutation.CreatePresignedURL.Headers != "{}" ||
+		mutation.CreatePresignedURL.Method != "post" {
+		return nil, fmt.Errorf("unsupported attachment API")
+	}
+	var fields map[string]string
+	err = json.Unmarshal([]byte(mutation.CreatePresignedURL.Fields), &fields)
+	if err != nil {
+		return nil, err
+	}
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+	for key, value := range fields {
+		fw, err := w.CreateFormField(key)
+		if err != nil {
+			return nil, err
+		}
+		_, err = fw.Write([]byte(value))
+		if err != nil {
+			return nil, err
+		}
+	}
+	fw, err := w.CreateFormFile("file", attachment.Filename)
+	if err != nil {
+		return nil, err
+	}
+	_, err = fw.Write(attachment.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	w.Close()
+	req, err := http.NewRequest("POST", mutation.CreatePresignedURL.URL, &b)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 204 {
+		return nil, fmt.Errorf("invalid status code: %d", res.StatusCode)
+	}
+	return &wiptypes.AttachmentInput{
+		Key:      fields["key"],
+		Filename: attachment.Filename,
+		Size:     len(attachment.Bytes),
+	}, nil
+}
+
 func (c Client) MutateCreateTodo(ctx context.Context, body string, completedAt *time.Time, attachments []Attachment) (*wiptypes.CreateTodoMutation, error) {
 	if !c.hasKey {
 		return nil, ErrTokenRequired
 	}
 	attachmentsInput := make([]wiptypes.AttachmentInput, len(attachments))
 	for i, attachment := range attachments {
-		var mutation wiptypes.CreatePresignedURLMutation
-		variables := map[string]interface{}{
-			"filename": graphql.String(attachment.Filename),
-		}
-		err := c.graphql.Mutate(ctx, &mutation, variables)
+		attachmentInput, err := c.uploadAttachment(ctx, attachment)
 		if err != nil {
 			return nil, err
 		}
-		if mutation.CreatePresignedURL.Headers != "{}" ||
-			mutation.CreatePresignedURL.Method != "post" {
-			return nil, fmt.Errorf("unsupported attachment API")
-		}
-		var fields map[string]string
-		err = json.Unmarshal([]byte(mutation.CreatePresignedURL.Fields), &fields)
-		if err != nil {
-			return nil, err
-		}
-		var b bytes.Buffer
-		w := multipart.NewWriter(&b)
-		for key, value := range fields {
-			fw, err := w.CreateFormField(key)
-			if err != nil {
-				return nil, err
-			}
-			_, err = fw.Write([]byte(value))
-			if err != nil {
-				return nil, err
-			}
-		}
-		fw, err := w.CreateFormFile("file", attachment.Filename)
-		if err != nil {
-			return nil, err
-		}
-		_, err = fw.Write(attachment.Bytes)
-		if err != nil {
-			return nil, err
-		}
-		w.Close()
-		req, err := http.NewRequest("POST", mutation.CreatePresignedURL.URL, &b)
-		if err != nil {
-			return nil, err
-		}
-		req.Header.Set("Content-Type", w.FormDataContentType())
-		res, err := http.DefaultClient.Do(req)
-		if err != nil {
-			return nil, err
-		}
-		defer res.Body.Close()
-		if res.StatusCode != 204 {
-			return nil, fmt.Errorf("invalid status code: %d", res.StatusCode)
-		}
-		attachmentsInput[i] = wiptypes.AttachmentInput{
-			Key:      fields["key"],
-			Filename: attachment.Filename,
-			Size:     len(attachment.Bytes),
-		}
+		attachmentsInput[i] = *attachmentInput
 	}
 	var mutation wiptypes.CreateTodoMutation
 	variables := map[string]interface{}{
